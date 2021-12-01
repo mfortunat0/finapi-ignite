b928b97f6a1bda29eb1aa77c8c7be4c2f027c0ba
matheus <matheus.xmaz10@gmail.com>
2021-11-30 22:25:58 -0300

    middlewares

--- src/index.js
+++ src/index.js
@@ -5,6 +5,19 @@ const { v4: uuidv4 } = require("uuid");
 app.use(express.json());
 const customers = [];
 
+function verifyIfExistsAccountCPF(request, response, next) {
+  const { cpf } = request.headers;
+  const customer = customers.find((customer) => customer.cpf === cpf);
+  if (!customer) {
+    response.status(400).json({
+      error: "Customer not found",
+    });
+  }
+
+  request.customer = customer;
+  return next();
+}
+
 app.post("/account", (request, response) => {
   const { cpf, name } = request.body;
   const customersAlreadyExists = customers.some(
@@ -27,14 +40,8 @@ app.post("/account", (request, response) => {
   return response.status(201).send();
 });
 
-app.get("/statement/", (request, response) => {
-  const { cpf } = request.headers;
-  const customer = customers.find((customer) => customer.cpf === cpf);
-  if (!customer) {
-    response.status(400).json({
-      error: "Customer not found",
-    });
-  }
+app.get("/statement/", verifyIfExistsAccountCPF, (request, response) => {
+  const { customer } = request;
   return response.json(customer.statement);
 });
 
